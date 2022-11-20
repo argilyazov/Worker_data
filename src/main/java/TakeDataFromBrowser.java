eef5f2092117251a98f89ca783888ca959d8abd7
arsel <arsel.Giliazov@urfu.me>
2022-11-20 16:37:40 +0500

    TakeDataFromBrowser - собирает данные с сайта с работниками

--- src/main/java/TakeDataFromBrowser.java
+++ src/main/java/TakeDataFromBrowser.java
@@ -1,55 +1,35 @@
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class TakeDataFromBrowser {
-    private static final String TARGET_URL = "https://www.avito.ru/ekaterinburg/noutbuki?cd=1&s=2";
-    private static final String outputPath = "texts_from_web";
+    private static final String TARGET_URL = "https://ekaterinburg.careerist.ru/cvs-ofitsiant/";
     private static final String USER_AGENT = "Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6";
 
-    public static void func() {
+    public static WorkerManager getWaitersFromBrowser() {
+        WorkerManager waiters = new WorkerManager();
         try {
-            Document document = Jsoup.connect(TARGET_URL).userAgent(USER_AGENT).referrer("http://www.google.com").get();
-            Elements elements = document.select("div.b-personnel.t-big_first.t-reg");
-            List<String> lines = new ArrayList<>();
-
-            elements.forEach(element -> {
-                String class_a = element.attr("class");
-                var s =element.text();
-                if (s.contains("Шорнинг"))
-                    System.out.println(s);
-                if (class_a.equals("b-personnel__card-title empty_info")) {
-                    lines.add(element.text());
-                }
-            });
-
-            Path file = Paths.get("warning.txt");
-            try {
-                Files.write(file, lines, StandardCharsets.UTF_8);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            var document = Jsoup.connect(TARGET_URL).userAgent(USER_AGENT).referrer("http://www.google.com").get();
+            for (var i = 4; i <= 33; i++) {
+                var nameSurname = document
+                        .select(String.format("#resume-tag > div > div:nth-child(%s) > div.list-block > div.block-with-big-foto > div.pull-md-left > p", i))
+                        .get(0).text() + " -";
+                var ages = document
+                        .select(String.format("#resume-tag > div > div:nth-child(%s) > div.list-block > div.block-with-big-foto > div.pull-md-left > div > p:nth-child(5)", i));
+                var age = ages.size() == 0 ? "-1" : ages.get(0).text();
+                var exps = document
+                        .select(String.format("#resume-tag > div > div:nth-child(%s) > div.list-block > div.block-with-big-foto > div.pull-md-left > div > p:nth-child(8)", i));
+                var exp = exps.size() == 0 ? "Без опыта" : exps.get(0).text();
+                exp = exp.equals("Без опыта") ? "0" : exp.split(" ")[0];
+                waiters.addWorker(new Waiter(i, nameSurname.split(" ")[0], nameSurname.split(" ")[1],
+                        Integer.parseInt(age.split(" ")[0]),
+                        Integer.parseInt(exp),
+                        i * 10 + i % 3, 24));
             }
+            return waiters;
+        } catch (Exception e) {
+            throw new RuntimeException("Error conection, data don't download");
         }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static void func1() {
-
     }
 }
+
 
 
